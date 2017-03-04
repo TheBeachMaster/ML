d9bf40b7d7f7bdd2d20bb6fd662ce89431ef564e
TheBeachMaster <otienoken@outlook.com>
2017-03-04 08:22:38 +0300

    Unsuccessfully tried the Mandelbrot example

--- Tensorflow/KU/tensorboard/tboardv2/tboardt.py
+++ Tensorflow/KU/tensorboard/tboardv2/tboardt.py
@@ -1,80 +1,83 @@
-# Import MNIST data
-import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
 import tensorflow as tf
 
-# Set parameters
-learning_rate = 0.01
-training_iteration = 30
+# reset everything to rerun in jupyter
+tf.reset_default_graph()
+
+# config
 batch_size = 100
-display_step = 2
+learning_rate = 0.5
+training_epochs = 5
+logs_path = "/RepoMan/ML/Tensorflow/KU/tensorboard/tboardv2/logs"
 
-# TF graph input
-x = tf.placeholder("float", [None, 784]) # mnist data image of shape 28*28=784
-y = tf.placeholder("float", [None, 10]) # 0-9 digits recognition => 10 classes
+# load mnist data set
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-# Create a model
+# input images
+with tf.name_scope('input'):
+    # None -> batch size can be any size, 784 -> flattened mnist image
+    x = tf.placeholder(tf.float32, shape=[None, 784], name="x-input") 
+    # target 10 output classes
+    y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y-input")
 
-# Set model weights
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+# model parameters will change during training so we use tf.Variable
+with tf.name_scope("weights"):
+    W = tf.Variable(tf.zeros([784, 10]))
 
-with tf.name_scope("Wx_b") as scope:
-    # Construct a linear model
-    model = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+# bias
+with tf.name_scope("biases"):
+    b = tf.Variable(tf.zeros([10]))
+
+# implement model
+with tf.name_scope("softmax"):
+    # y is our prediction
+    y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+# specify cost function
+with tf.name_scope('cross_entropy'):
+    # this is our cost
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# specify optimizer
+with tf.name_scope('train'):
+    # optimizer is an "operation" which we can execute in a session
+    train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+
+with tf.name_scope('Accuracy'):
+    # Accuracy
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     
-# Add summary ops to collect data
-w_h = tf.histogram_summary("weights", W)
-b_h = tf.histogram_summary("biases", b)
+# create a summary for our cost and accuracy
+tf.scalar_summary("cost", cross_entropy)
+tf.scalar_summary("accuracy", accuracy)
 
-# More name scopes will clean up graph representation
-with tf.name_scope("cost_function") as scope:
-    # Minimize error using cross entropy
-    # Cross entropy
-    cost_function = -tf.reduce_sum(y*tf.log(model))
-    # Create a summary to monitor the cost function
-    tf.scalar_summary("cost_function", cost_function)
+# merge all summaries into a single "operation" which we can execute in a session 
+summary_op = tf.merge_all_summaries()
 
-with tf.name_scope("train") as scope:
-    # Gradient descent
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
-
-# Initializing the variables
-init = tf.initialize_all_variables()
-
-# Merge all summaries into a single operator
-merged_summary_op = tf.merge_all_summaries()
-
-# Launch the graph
 with tf.Session() as sess:
-    sess.run(init)
-    
-    # Set the logs writer to the folder /tmp/tensorflow_logs
-    summary_writer = tf.train.SummaryWriter('/logs', graph_def=sess.graph_def)
+    # variables need to be initialized before we can use them
+    sess.run(tf.initialize_all_variables())
 
-    # Training cycle
-    for iteration in range(training_iteration):
-        avg_cost = 0.
-        total_batch = int(mnist.train.num_examples/batch_size)
-        # Loop over all batches
-        for i in range(total_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            # Fit training using batch data
-            sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
-            # Compute the average loss
-            avg_cost += sess.run(cost_function, feed_dict={x: batch_xs, y: batch_ys})/total_batch
-            # Write logs for each iteration
-            summary_str = sess.run(merged_summary_op, feed_dict={x: batch_xs, y: batch_ys})
-            summary_writer.add_summary(summary_str, iteration*total_batch + i)
-        # Display logs per iteration step
-        if iteration % display_step == 0:
-            print "Iteration:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(avg_cost)
-
-    print "Tuning completed!"
-
-    # Test the model
-    predictions = tf.equal(tf.argmax(model, 1), tf.argmax(y, 1))
-    # Calculate accuracy
-    accuracy = tf.reduce_mean(tf.cast(predictions, "float"))
-    print "Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels})
+    # create log writer object
+    writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
+        
+    # perform training cycles
+    for epoch in range(training_epochs):
+        
+        # number of batches in one epoch
+        batch_count = int(mnist.train.num_examples/batch_size)
+        
+        for i in range(batch_count):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            
+            # perform the operations we defined earlier on batch
+            _, summary = sess.run([train_op, summary_op], feed_dict={x: batch_x, y_: batch_y})
+            
+            # write log
+            writer.add_summary(summary, epoch * batch_count + i)
+            
+        if epoch % 5 == 0: 
+            print "Epoch: ", epoch 
+    print "Accuracy: ", accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+    print "done"
