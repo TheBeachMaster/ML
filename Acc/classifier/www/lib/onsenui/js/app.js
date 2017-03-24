e50bc45e1776cf7b4b6b26821a5c88ba5e7bb75f
TheBeachMaster <otienoken@outlook.com>
2017-03-25 00:27:57 +0300

    Logic heade somewhere

--- Acc/classifier/www/lib/onsenui/js/app.js
+++ Acc/classifier/www/lib/onsenui/js/app.js
@@ -1,10 +1,15 @@
-var submit = function() {
-  var data = document.getElementById('input').value;
-  
-  if (data != '') {
-    ons.notification.alert('Congratulations!');
-  }
-  else {
-    ons.notification.alert('We do not take empty words');
-  }
+var submit = function () {
+    var data = document.getElementById('input').value;
+
+    if (data != '') {
+        ons.notification.alert('Congratulations!');
+
+        var content = document.getElementById("classif");
+        content.innerHTML = "<ons-list-item modifier=\"longdivider\">Item B</ons-list-item>";
+
+
+    }
+    else {
+        ons.notification.alert('We do not take empty words');
+    }
 };
