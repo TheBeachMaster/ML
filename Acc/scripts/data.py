1b76378a5b620232ab5efac7b07a2fdbbfc28cee
TheBeachMaster <otienoken@outlook.com>
2017-03-25 11:54:02 +0300

    Unusable code

--- Acc/scripts/data.py
+++ Acc/scripts/data.py
@@ -11,5 +11,3 @@ data = '{"texts":["yeah right"]}'
 
 requests.post('https://api.uclassify.com/v1/ArthurKenOtieno/democlassifier/classify', headers=headers, data=data)
 
-parsed_json = json.loads(json_string)
-print(parsed_json['first_name'])
